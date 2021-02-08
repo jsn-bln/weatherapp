e4b899a5870bacf4059231fe0475430e6a1c5ad8
Jayson Balano <jaysonbalano99@gmail.com>
2021-02-08 12:15:24 +0800

    fixed server issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cities = require('all-the-cities')
+
 const app = express();
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
 app.listen(PORT , () => console.log(`server running on Port: ${PORT}`))
